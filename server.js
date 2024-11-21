e266488a0caf799f5c080fcc3c59d8c9f66f7682
BankVisaruts <visarut.ik@gmail.com>
2024-11-21 21:25:15 +0700

    edit call api

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+const userController = require('./controllers/UserController')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/user/signIn', (req, res) => userController.signin(req, res));
 
 app.listen(3000, () => {
     console.log("API Server Running...");
-})
+});

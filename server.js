99610f6eede2f66bc1f002da9737011664aafcf7
BankVisaruts <visarut.ik@gmail.com>
2024-11-22 20:44:12 +0700

    Edit user signin

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const userController = require('./controllers/UserController')
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

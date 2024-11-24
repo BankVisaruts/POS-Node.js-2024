b24a060a96f6cc8d66609392e76d51152adaebf7
BankVisaruts <visarut.ik@gmail.com>
2024-11-24 18:07:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('./api/foodType/create', (req,res) => foodTypeController.create(req,res));
+app.get('/api/foodType/list', (req, res) => foodTypeController.list(req, res));
+app.post('/api/foodType/create', (req, res) => foodTypeController.create(req,res));
 app.post('/api/user/signIn', (req, res) => userController.signin(req, res));
 
 app.listen(3000, () => {

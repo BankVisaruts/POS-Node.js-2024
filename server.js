93176cfa3bad78c6655e6a7d0f6ef35645afc408
BankVisaruts <visarut.ik@gmail.com>
2024-11-26 20:00:51 +0700

    add api remove

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.delete('/api/foodType/remove/:id', (req, res) => foodTypeController.remove(req, res));
 app.get('/api/foodType/list', (req, res) => foodTypeController.list(req, res));
 app.post('/api/foodType/create', (req, res) => foodTypeController.create(req,res));
 app.post('/api/user/signIn', (req, res) => userController.signin(req, res));

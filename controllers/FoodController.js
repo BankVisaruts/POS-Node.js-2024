32c48103655a057429ecc4ae1731db58f94a9934
BankVisaruts <visarut.ik@gmail.com>
2024-12-07 13:59:01 +0700

    Update FoodController.js

--- controllers/FoodController.js
+++ controllers/FoodController.js
@@ -20,5 +20,60 @@ module.exports = {
         } catch (e) {
             return res.status(500).send({ error: e.message })
         }
+    },
+    upload: async (req, res) => {
+        try {
+           if (req.files.img !== undefined) {
+            const img = req.files.img;
+            const fileName = img.name;
+
+            img.mv('uploads/' + fileName, (err) => {
+                if (err) {
+                    res.send({ error: err })
+                }
+            })
+
+            return res.send({ fileName: fileName })
+           } else {
+            return res.send({ message: 'file not found' })
+           }
+        } catch (e) {
+            return res.status(500).send({ error: e.message })
+        }
+    },
+    list: async (req, res) => {
+        try {
+           const rows = await prisma.food.findMany({
+            include: {
+                FoodType: true,
+            },
+            orderBy: {
+                id: 'desc',
+            },
+            where: {
+                status: 'use',
+            },
+           });
+
+           return res.send({ results: rows });
+        } catch (e) {
+            return res.status(500).send({ error: e.message })
+        }
+    },
+    remove: async (req, res) => {
+        try {
+            await prisma.food.update({
+                data: {
+                    status: 'delete',
+                },
+                where: {
+                    id: parseInt(req.params.id),
+                },
+            });
+
+            return res.send({ message: "success" });
+        } catch (e) {
+            return res.status(500).send({ error: e.message })
+        }
     }
 }

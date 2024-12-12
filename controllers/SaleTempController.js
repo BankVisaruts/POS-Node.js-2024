82e6e9c7b48115dfc3a9ee67e177518900fa3293
BankVisaruts <visarut.ik@gmail.com>
2024-12-12 19:49:28 +0700

    Create SaleTempController.js

---
+++ controllers/SaleTempController.js
@@ -0,0 +1,29 @@
+const { PrismaClient } = require('@prisma/client');
+const { PrismaClientExtends } = require('@prisma/client/extension');
+const prisma = new PrismaClient();
+
+module.exports = {
+    create: async (req, res) => {
+        try {
+            const row = await prisma.food.findFirst({
+                where: {
+                    id: req.body.foodId
+                }
+            });
+    
+            await prisma.saleTemp.create({
+                data: {
+                    foodId: req.body.foodId,
+                    qty: req.body.qty,
+                    price: row.price,
+                    userId: req.body.userId,
+                    tableNo: req.body.tableNo
+                }
+            });
+
+            return res.send({ message: "success" });
+        } catch (e) {
+            return res.status(500).send({ error: e.message });
+        }
+    }
+}
